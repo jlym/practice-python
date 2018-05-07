3c3342b82d9894d7690069754beaef0f88a25536
Jaclyn Olmos-Silverman <jaclyn.olmossilverman@gmail.com>
2018-05-07 01:51:23 +0000

    Implement prime.py

---
+++ hw/week4/prime.py
@@ -0,0 +1,15 @@
+def is_prime(n):
+    """Returns True is number is prime, and false otherwise."""
+    
+    counter = 2
+    if n < counter:
+        return False
+    while counter < n:
+        if n%counter == 0:
+            return False
+        else:
+            counter+=1
+    return True
+ 
+        
+        
